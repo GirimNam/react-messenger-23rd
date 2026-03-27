93219437776ae25220fcc192a088f616cf1dd539
girimNam <rlfla@naver.com>
2026-03-28 02:42:20 +0900

    fix: 스크롤 기능 수정

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,19 +1,36 @@
-import ChatList from '@Chat/ChatList'
+import { useChat } from '@/hooks/useChat'
+import ChatMessage from '@Chat/ChatMessage'
 import ChatInput from '@Chat/ChatInput'
-import ChatHeader from '@Chat/ChatHeader'
-import type { Message } from '@/types/chat'
+import ChatHeader from '@/Chat/ChatHeader'
 
-const dummyData: Message[] = [
-  { id: '1', text: '안녕', sender: 'other', time: '12:00' },
-  { id: '2', text: 'ㅇㅇ', sender: 'me', time: '12:01' },
-]
+export default function Chat() {
+  const { chatList, sendMessage } = useChat()
 
-export default function ChatPage() {
   return (
-    <div className="app">
-      <ChatHeader title="Data Visualization" />
-      <ChatList messages={dummyData} />
-      <ChatInput />
+    <div className="flex flex-col flex-1 overflow-hidden">
+      <ChatHeader title="Data Visulization" />
+
+      {/* 채팅 리스트 */}
+      <div className="flex-1 overflow-y-auto no-scrollbar px-3 py-2 space-y-2">
+        {chatList.map((chat, index) => {
+          const next = chatList[index + 1]
+
+          const showTime =
+            !next || // 마지막 메시지
+            next.sender !== chat.sender // 다음 메시지가 다른 사람
+
+          return (
+            <ChatMessage
+              key={chat.id}
+              message={chat}
+              showTime={showTime}
+            />
+          )
+        })}
+      </div>
+
+      {/* 입력 */}
+      <ChatInput onSend={sendMessage} />
     </div>
   )
 }
