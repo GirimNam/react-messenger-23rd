bc8ef6a43702d29548d0c4ae5d702940d92466f3
girimNam <rlfla@naver.com>
2026-04-13 10:40:52 +0900

    feat: 공통 레이아웃 지정

---
+++ src/layout/PublicLayout.tsx
@@ -0,0 +1,20 @@
+import { Outlet, useLocation } from 'react-router-dom'
+import NavBar from '@/components/NavBar'
+
+function PublicLayout() {
+  const location = useLocation()
+
+  const showNav = ['/myprofile', '/chatlist'].includes(location.pathname)
+
+  return (
+    <div className="app">
+      <main>
+        <Outlet />
+      </main>
+
+      {showNav && <NavBar />}
+    </div>
+  )
+}
+
+export default PublicLayout
