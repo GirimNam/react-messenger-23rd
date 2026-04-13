8860a7fa5aaf0f7bb681f17ceb58eb1ff33e481d
girimNam <rlfla@naver.com>
2026-04-13 10:37:07 +0900

    chore: router 및 svg 관련 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,30 @@ import Chat from '@/pages/Chat'
 import ChatList from '@/pages/ChatList'
 import Friends from '@/pages/Friends'
 import MyProfile from '@/pages/MyProfile'
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
   return (
     <div className="app">
-      <Chat />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/myprofile"
+            element={<MyProfile />}
+          />
+          <Route
+            path="/friends"
+            element={<Friends />}
+          />
+          <Route
+            path="/chatlist"
+            element={<ChatList />}
+          />
+          <Route
+            path="/chat"
+            element={<Chat />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
