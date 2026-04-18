9ca2c987f8ce1b6475f9982ab9dfe8eeb58ff9da
girimNam <rlfla@naver.com>
2026-04-18 15:42:32 +0900

    chore: tailwind 및 기본 내용 변경

--- src/pages/LoadingPage.tsx
+++ src/pages/LoadingPage.tsx
@@ -2,7 +2,7 @@ function LoadingPage() {
   return (
     <div className="flex flex-col items-center justify-center h-full gap-5">
       <div className="w-14 h-14 rounded-full border-[5px] border-teal-60 border-t-transparent animate-spin" />
-      <p className="text-body2_m text-gray-70 antialiased">로딩 중</p>
+      <p className="text-body2_m text-gray-95 antialiased">로딩 중</p>
     </div>
   )
 }
