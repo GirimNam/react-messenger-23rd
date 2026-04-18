141abf2afce9f5b83054577bd5db3598ad58e58a
girimNam <rlfla@naver.com>
2026-04-18 15:43:25 +0900

    fix: 즐겨찾기 설정 시 프로필 사진 별 추가

--- src/components/OtherProfile.tsx
+++ src/components/OtherProfile.tsx
@@ -33,7 +33,9 @@ function OtherProfile({ profile }: { profile: Profile }) {
             alt="즐겨찾기 아이콘"
             className="w-5 h-5"
           />
-          <span className="text-caption2_sb antialiased whitespace-nowrap">즐겨찾기</span>
+          <span className="text-caption2_sb antialiased whitespace-nowrap">
+            즐겨찾기
+          </span>
         </button>
       </div>
 
@@ -43,10 +45,24 @@ function OtherProfile({ profile }: { profile: Profile }) {
           isActionOpen ? 'translate-x-18.5' : 'translate-x-0'
         }`}
       >
-        <img
-          src={Profile3}
-          alt="프로필사진"
-        />
+        <div className="relative">
+          <img
+            src={Profile3}
+            alt="프로필사진"
+          />
+          {profile.favorite && (
+            <svg
+              viewBox="0 0 24 24"
+              className="absolute -top-1 -left-1 w-5 h-5"
+              aria-label="즐겨찾기"
+            >
+              <path
+                d="M8.85044 8.01306L4.42919 9.48606C4.33302 9.51806 4.26735 9.58381 4.23219 9.68331C4.19685 9.78264 4.21127 9.87398 4.27544 9.95731L7.14394 13.9161L7.03694 18.2323C7.03044 18.3413 7.06885 18.4278 7.15219 18.4918C7.23552 18.556 7.32852 18.5721 7.43119 18.5401L11.9984 17.2226L16.5907 18.5151C16.6934 18.5471 16.7864 18.531 16.8697 18.4668C16.953 18.4028 16.9914 18.3163 16.9849 18.2073L16.8504 13.8901L19.7214 9.93231C19.7856 9.84898 19.8 9.75764 19.7647 9.65831C19.7295 9.55881 19.6639 9.49306 19.5677 9.46106L15.1214 8.01306L12.2389 4.28431C12.1811 4.20081 12.1009 4.15906 11.9984 4.15906C11.8959 4.15906 11.8158 4.20081 11.7579 4.28431L8.85044 8.01306Z"
+                fill="#207BD5"
+              />
+            </svg>
+          )}
+        </div>
 
         <div className="flex flex-col">
           <p className="text-body2_m antialiased">{profile.name}</p>
